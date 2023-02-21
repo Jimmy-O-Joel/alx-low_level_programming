81e1a3bf3d58790ad3b2901c95ee4af11ae623c0
Jimmy-O-Joel <jimmyjoel177@gmail.com>
2023-02-21 06:43:41 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
 * main - Prints numbers between 00 to 99.
@@ -9,7 +8,16 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar("\n");
 	return (0);
 }
 
