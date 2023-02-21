d931723f31d763cfbaeed70737430c5a0c90aaec
Jimmy-O-Joel <jimmyjoel177@gmail.com>
2023-02-21 05:52:59 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Prints numbers between 00 to 99.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
