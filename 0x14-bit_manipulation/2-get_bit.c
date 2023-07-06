5409ee6f828cad2eb4d58a921666dd3122cb7b1f
Jimmy-O-Joel <jimmyjoel177@gmail.com>
2023-07-06 20:39:15 +0300

    2-get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,15 +12,16 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int mask = 1 << index;
+	unsigned int i;
 
 	if (n == 0 && index < 64)
 		return (0);
 
-	if (mask & n)
-		return (1);
-	else
-		return (0);
-
+	for (i = 0; i < 64; n >>= 1, i++)
+	{
+		if (index == i)
+			return (n & 1);
+	}
 	return (-1);
 }
+
